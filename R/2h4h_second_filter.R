9bd61d3de291017ef2b12cf6b3132ba59befeaac
Taylor Spaulding <tspaulding@esassoc.com>
2025-06-10 12:48:26 -0700

    fix Tag_Hex

--- R/2h4h_second_filter.R
+++ R/2h4h_second_filter.R
@@ -27,7 +27,7 @@ second_filter_2h4h <- function(fish_file){
   spur <- compare_detects(fish_file, final_file)
 
   message(paste0("Number of Tags with Spurious Detections: ", length(unique(spur$Tag_Hex))))
-  message(paste0("Tags with Only Spurious Detections: "), spur$TagHex[which(!(unique(spur$Tag_Hex) %in% (unique(final_file$Tag_Hex))))])
+  message(paste0("Tags with Only Spurious Detections: "), spur$Tag_Hex[which(!(unique(spur$Tag_Hex) %in% (unique(final_file$Tag_Hex))))])
   message(paste0("Number of Spurious Detections: ", length(spur$DateTime_Local)))
 
   final_file
