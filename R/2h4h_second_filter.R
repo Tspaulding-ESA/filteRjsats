1f6597d802e5bbe9de85af689e7804479e54c52f
Taylor Spaulding <tspaulding@esassoc.com>
2025-08-05 07:20:12 -0700

    New message and skip file if no detections found in fish file

--- R/2h4h_second_filter.R
+++ R/2h4h_second_filter.R
@@ -14,21 +14,26 @@
 #' second_filter_2h4h(filter_fish_detects)
 second_filter_2h4h <- function(fish_file){
 
-  make = fish_file$Make[1]
-  final_file = NULL
-  if (make == "Lotek") final_file <- second_filter_lotek(fish_file)
-  if (make == "Tekno") final_file <- second_filter_tekno(fish_file)
-  if (make == "ATS")   final_file <- second_filter_ats(fish_file)
-  if (is.null(final_file)) errorCondition("Receiver make should be one of Lotek, Tekno, or ATS")
+  if (nrow(fish_file) > 0) {
+    make = fish_file$Make[1]
+    final_file = NULL
+    if (make == "Lotek") final_file <- second_filter_lotek(fish_file)
+    if (make == "Tekno") final_file <- second_filter_tekno(fish_file)
+    if (make == "ATS")   final_file <- second_filter_ats(fish_file)
+    if (is.null(final_file)) errorCondition("Receiver make should be one of Lotek, Tekno, or ATS")
 
-  message(paste0("Number of Valid Tag IDs: ", length(unique(final_file$Tag_Hex))))
-  message(paste0("Number of Valid Detections: ", length(final_file$DateTime_Local)))
+    message(paste0("Number of Valid Tag IDs: ", length(unique(final_file$Tag_Hex))))
+    message(paste0("Number of Valid Detections: ", length(final_file$DateTime_Local)))
 
-  spur <- compare_detects(fish_file, final_file)
+    spur <- compare_detects(fish_file, final_file)
 
-  message(paste0("Number of Tags with Spurious Detections: ", length(unique(spur$Tag_Hex))))
-  message(paste0("Tags with Only Spurious Detections: "), spur$Tag_Hex[which(!(unique(spur$Tag_Hex) %in% (unique(final_file$Tag_Hex))))])
-  message(paste0("Number of Spurious Detections: ", length(spur$DateTime_Local)))
+    message(paste0("Number of Tags with Spurious Detections: ", length(unique(spur$Tag_Hex))))
+    message(paste0("Tags with Only Spurious Detections: "), spur$Tag_Hex[which(!(unique(spur$Tag_Hex) %in% (unique(final_file$Tag_Hex))))])
+    message(paste0("Number of Spurious Detections: ", length(spur$DateTime_Local)))
 
-  final_file
+    final_file
+    } else {
+      message("No Valid Detections in File, Skipping File")
+      NULL
+    }
 }
